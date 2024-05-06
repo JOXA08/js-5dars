956e6a2b87519cc57e39d7f0924c3eded3e38824
Jahongir Safarov <168184861+JOXA08@users.noreply.github.com>
2024-05-06 17:25:37 +0500

    js 5dars
    zeromax itcentr

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/*
+let headTag = document.head;
+let bodyTag = document.body;
+let havola = document.documentURI
+let formalar = document.forms
+let rasmlar = document.images
+let havolalar = document.links
+console.log(havolalar);
+*/
+
+/* Events in JS: onclic*/
+
+/*function battonniBos() {
+    alert("San battonni bosding!")
+}
+function battonniIKKImartaBOS() {
+    alert("battonni 2 marta bosding")
+}
+function tanlash(event) {
+    alert(event.target.value + "ni tanladingiz");
+}*/
+/*function inputgaBosish() {
+    console.log("inputga bosildi")
+}
+function inputdanCHiqish() {
+    console.log("inputdan cqld")
+}
+function yozish(event) {
+    console.log(event.target.value);
+}
+document.querySelector(".text").addEventListener("copy",function (event) {
+    console.log("DIQQAT " + event.target.value + "nomli fayl kochirildi");
+})*/
+
+function harakatlanish(e) {
+    console.log(e.clientX, e.clientY);
+}
+function kirish() {
+    alert("hududga kirildi")
+}
+function chiqish() {
+    alert("hududdan chqildi")
+}
+
